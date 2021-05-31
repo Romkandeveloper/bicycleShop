02b04027a016bcd2410fbb1a732a13159f7fb6c4
Roman Chaban <romanchaban9@gmail.com>
2021-05-31 20:02:26 +0300

    Hi

---
+++ code/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.main__list').slick({
+        slidesToShow: 2,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        draggable: true,
+    });
+});
